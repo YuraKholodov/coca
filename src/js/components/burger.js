245d403f2ee8588160bb16e19d99b55ba0b11c0d
Yura Kholodov <yurdast@mail.ru>
2024-08-12 15:01:17 +0300

    added hero section

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -2,11 +2,13 @@ export function useBurger() {
   const burgerBtn = document.querySelector('[data-burger="btn"]');
   const burgerMenu = document.querySelector('[data-burger="menu"]');
   const overlay = document.querySelector('[data-burger="overlay"]');
+  const body = document.body;
 
   function closeBurgerMenu() {
     burgerBtn.classList.remove('burger--active');
     overlay.classList.remove('overlay--visible');
     burgerMenu.classList.remove('header__right--visible');
+    body.classList.remove('body--fixed');
   }
 
   overlay.addEventListener('click', closeBurgerMenu);
@@ -15,5 +17,6 @@ export function useBurger() {
     burgerBtn.classList.toggle('burger--active');
     overlay.classList.toggle('overlay--visible');
     burgerMenu.classList.toggle('header__right--visible');
+    body.classList.toggle('body--fixed');
   });
 }
