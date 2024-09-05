4444b9977647f4419f5db948605cf5bcba0ea051
Yura Kholodov <yurdast@mail.ru>
2024-09-05 08:35:54 +0300

    created insight

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -3,11 +3,18 @@ import "swiper/css/bundle";
 
 export const insightSlider = () => {
   new Swiper(".swiper", {
-    slidesPerView: 2,
-    spaceBetween: 30,
+    slidesPerView: 1.2,
+    spaceBetween: 20,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      577: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
     },
   });
 };
