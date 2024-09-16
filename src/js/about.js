4b1d6b37b508eb35aed82b4869e67e8dd3e8d2eb
Yura Kholodov <yurdast@mail.ru>
2024-09-16 14:41:17 +0300

    created about

--- src/js/about.js
+++ src/js/about.js
@@ -2,10 +2,11 @@ import { useTheme } from "./components/theme.js";
 import { useBurger } from "./components/burger.js";
 import { headerScroll } from "./components/header.js";
 import { footerAccordion } from "./components/footer-accordion.js";
-import { heroSlider } from "./components/about/slider.js";
+import { heroSlider, teamSlider } from "./components/about/slider.js";
 
 useTheme();
 useBurger();
 headerScroll();
 footerAccordion();
 heroSlider();
+teamSlider();
--- src/js/components/about/slider.js
+++ src/js/components/about/slider.js
@@ -3,11 +3,42 @@ import "swiper/css/bundle";
 
 export const heroSlider = () => {
   new Swiper(".hero__slider", {
-    slidesPerView: 2.25,
+    slidesPerView: 1.25,
     spaceBetween: 32,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 32,
+      },
+      1200: {
+        slidesPerView: 2.25,
+        spaceBetween: 32,
+      },
+    },
+  });
+};
+
+export const teamSlider = () => {
+  new Swiper(".team__slider", {
+    slidesPerView: 1.25,
+    spaceBetween: 32,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 32,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 32,
+      },
     },
   });
 };
