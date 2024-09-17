b45dc11a77658806a05ba8e2127a918983334649
Yura Kholodov <yurdast@mail.ru>
2024-09-17 16:50:03 +0300

    created blog

--- src/js/blog.js
+++ src/js/blog.js
@@ -2,7 +2,7 @@ import { useTheme } from "./components/theme.js";
 import { useBurger } from "./components/burger.js";
 import { headerScroll } from "./components/header.js";
 import { footerAccordion } from "./components/footer-accordion.js";
-import { activitySlider } from "./components/activity/slider.js";
+import { activitySlider } from "./components/blog/slider.js";
 import { mixer } from "./components/mixitUP.js";
 
 useTheme();
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const config = {
     pricing: "./src/js/pricing.js",
     contact: "./src/js/contact.js",
     about: "./src/js/about.js",
-    activity: "./src/js/activity.js",
+    blog: "./src/js/blog.js",
   },
   output: {
     filename: "[name].bundle.js",
